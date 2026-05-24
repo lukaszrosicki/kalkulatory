4c6837b13f89fa247433fd57d82abb09d634c679
Lukasz <rosicki.lukasz@gmail.com>
2026-05-24 18:51:28 +0200

    Próbny deploy 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
   integrations: [react()],
   site: 'https://lukaszrosicki.github.io',
   base: '/kalkulatory/',
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      // Wymuszamy, by zasoby lądowały w jednym, sztywnym katalogu
+      assetsDir: '_astro',
+    }
+  }
 });
